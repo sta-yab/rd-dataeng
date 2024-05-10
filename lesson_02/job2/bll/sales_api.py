7349f0d39991452da44b610a0ff2f37eec070698
sta-yab <71318818+sta-yab@users.noreply.github.com>
2024-05-10 17:56:30 +0200

    Delete lesson_02 directory

--- lesson_02/job2/bll/sales_api.py
+++
@@ -1,9 +0,0 @@
-from dal.local_disk import save_to_disk
-from dal.sales_api import get_sales
-
-
-def save_sales_to_local_disk(date: str, raw_dir: str) -> None:
-    # 1. get data from the API
-    # 2. save data to disk
-    data = get_sales(date)
-    save_to_disk(data, raw_dir)
